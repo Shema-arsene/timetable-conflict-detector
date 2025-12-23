2cfe53ee48cecd0e8c4751a456f6edffe849e9c4
Shema Arsene <aniyonkurushema@gmail.com>
2025-12-23 12:54:21 +0200

    Create pages to manage rooms and lecturers

---
+++ app/lecturers/page.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const page = () => {
+  return (
+    <section className="h-screen flex items-center justify-center">
+      Lecturers page
+    </section>
+  )
+}
+
+export default page
---
+++ app/rooms/page.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const page = () => {
+  return (
+    <section className="h-screen flex items-center justify-center">
+      Rooms page
+    </section>
+  )
+}
+
+export default page
