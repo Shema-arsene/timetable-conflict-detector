456db420edfc9a2646a06ba496526b363a551045
Shema Arsene <aniyonkurushema@gmail.com>
2026-03-19 00:37:43 +0200

    Add new routes to the app routes

--- backend/src/routes/timetable.routes.ts
+++ backend/src/routes/timetable.routes.ts
@@ -1,8 +1,18 @@
 import { Router } from "express"
-import { createTimetable } from "../controllers/timetable.controller.js"
+import {
+  createTimetable,
+  getTimetables,
+  getTimetableById,
+  updateTimetable,
+  deleteTimetable,
+} from "../controllers/timetable.controller.js"
 
 const router = Router()
 
 router.post("/", createTimetable)
+router.get("/", getTimetables)
+router.get("/:id", getTimetableById)
+router.put("/:id", updateTimetable)
+router.delete("/:id", deleteTimetable)
 
 export default router
